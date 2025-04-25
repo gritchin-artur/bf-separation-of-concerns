3fc6ff1bcbe29a3fc17f83ec52bdbc3da7b89b52
artur <grit4in0225@icloud.com>
2025-04-25 13:23:24 +0200

    finished rendering-data

--- 5-rendering-data/exercises-blanks/1-thumbnail-args-object/actual.js
+++ 5-rendering-data/exercises-blanks/1-thumbnail-args-object/actual.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const actual = (argsObj = {}) => {
-    const divEl = document.createElement('div');
-    divEl.className = 'thumbnail-container';
+  const divEl = document.createElement('div');
+  divEl.className = 'thumbnail-container';
 
-    const imgEl = document._('img');
-    imgEl._ = 'thumbnail to-the-right';
-    imgEl._ = argsObj.url;
-    imgEl.alt = `the color ${argsObj.color}`;
-    divEl._(imgEl);
+  const imgEl = document.createElement('img');
+  imgEl.classList = 'thumbnail to-the-right';
+  imgEl.src = argsObj.url;
+  imgEl.alt = `the color ${argsObj.color}`;
+  divEl.appendChild(imgEl);
 
-    return divEl;
+  return divEl;
 };
--- 5-rendering-data/exercises-blanks/5-virtual-dom-elements/actual.js
+++ 5-rendering-data/exercises-blanks/5-virtual-dom-elements/actual.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const actual = (virtualElement) => {
-    const { name, attributes = {}, children = [] } = virtualElement;
+  const { name, attributes = {}, children = [] } = virtualElement;
 
-    const el = document.createElement(_);
+  const el = document.createElement(name);
 
-    for (const attributeName in attributes) {
-        const attributeValue = attributes[_];
-        el.setAttribute(_, _);
+  for (const attributeName in attributes) {
+    const attributeValue = attributes[attributeName];
+    el.setAttribute(attributeName, attributeValue);
+  }
+
+  for (const child of children) {
+    if (typeof child === 'string') {
+      el.textContent += child;
+    } else {
+      // recursively render child elements
+      const renderedChild = actual(child);
+      el.appendChild(renderedChild);
     }
+  }
 
-    for (const child of children) {
-        if (typeof child === 'string') {
-            el.textContent += _;
-        } else {
-            // recursively render child elements
-            const renderedChild = actual(_);
-            el.appendChild(_);
-        }
-    }
-
-    return el;
+  return el;
 };
